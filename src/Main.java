8516682e1ef89f88c3bb564c935b848296863268
EosReign <107928418+EosReign@users.noreply.github.com>
2022-09-23 12:20:49 +0300

    *

--- src/Main.java
+++ src/Main.java
@@ -2,53 +2,45 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+    private static String regex = "\\w+[a-zA-Z_0-9]";
+    private static Pattern pattern = Pattern.compile(regex);
     public static void main(String[] args) {
         check("afasawfas@", ")fskfjsf", "sfanksf");
     }
     public static void check(String login, String password, String confirmPassword) {
         try {
             checkLogin(login);
+            checkPassword(password);
+            checkConfirmPassword(password, confirmPassword);
         } catch (WrongLoginException e) {
             System.err.println("Логин не соответствует требованиям");
-        } finally {
-            System.out.println("Проверка логина завершена");
-        }
-        try {
-            checkPassword(password);
         } catch (WrongPasswordException e) {
             System.err.println("Пороль не соответствует требованиям");
         } finally {
-            System.out.println("Проверка пороля завершена");
-        }
-        try {
-            checkConfirmPassword(password, confirmPassword);
-        } catch (WrongPasswordException e) {
-            System.err.println("Пороль не совпадает");
-        } finally {
-            System.out.println("Проверка поролей на совпадение завершена");
+            System.out.println("Проверка вводных данных завершена");
         }
     }
     public static void checkLogin(String login) throws WrongLoginException {
-        //Я черт знает, что я тут напечатал... работать должен, да вот ленится.
-        String regex = "^[a-zA-Z_0-9]";
-        Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(login);
 
-        if (login.length() >= 20 || login == "" || login == null) {
+        if (login == null || login == "" || login.length() >= 20) {
             throw new WrongLoginException("Длина логина не соответствует требованиям") ;
-        } else if (matcher.matches()) {
+        } else if (!matcher.matches()) {
             throw new WrongLoginException("Символы логина не соответствуют требованиям");
         }
     }
     public static void checkPassword(String password) throws WrongPasswordException{
-        if (password.length() >= 20) {
+        Matcher matcher = pattern.matcher(password);
+
+        if (password == null || password == "" || password.length() >= 20) {
             throw new WrongPasswordException("Длина пороля не соответствует требованиям");
+        } else if (!matcher.matches()) {
+            throw new WrongPasswordException("Символы пороля не соответствуют требованиям");
         }
     }
     public static void checkConfirmPassword(String password, String confirmPassword) throws WrongPasswordException{
-        if (password != confirmPassword) {
+        if (!password.equals(confirmPassword)) {
             throw new WrongPasswordException("Пороль не совпадает");
         }
     }
 }
-//..
